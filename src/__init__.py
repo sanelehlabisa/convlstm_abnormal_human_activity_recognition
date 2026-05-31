56a8ca1f3aae9a6b9cd79294f5871ddf53a1a238
shlabisa <shlabisa@sarao.ac.za>
2026-05-31 20:32:55 +0200

    Resolve circular imports by removing imports on the dander func

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dataset import Dataset
-from .evaluate import evaluate
-from .model import ConvLSTMModel
-from .train import train_one_epoch, validate_one_epoch
-from .utils import save_model, load_model, plot_confusion_matrix, plot_training_curves, save_prediction_clips
-
-__all__ = [
-    Dataset, evaluate, ConvLSTMModel, train_one_epoch, validate_one_epoch, save_model, load_model, plot_confusion_matrix, plot_training_curves, save_prediction_clips
-]
